ef55c24e52cd205c0811b632f0c60ead3b3d103e
fernandokkang <spineattack@gmail.com>
2024-01-17 13:13:21 +0900

    test. kakao map api test

--- js/map.js
+++ js/map.js
@@ -1,5 +1,3 @@
-//const key = kakaoMapApi.key;
-
 const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 const options = { //지도를 생성할 때 필요한 기본 옵션
         center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
