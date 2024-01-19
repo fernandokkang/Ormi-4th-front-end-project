7ae1fe8d9181d27a5820e2753a6262ffa563c8f2
fernandokkang <spineattack@gmail.com>
2024-01-19 11:53:55 +0900

    feat. 무한 스크롤 기능 추가

---
+++ js/showmore.js
@@ -0,0 +1,48 @@
+const showMoreBtn = document.querySelector('.Show-more-btn');
+const imgDiv = document.querySelector(".content-img-div");
+let pageToFetch = 1;
+let timer;
+
+showMoreBtn.addEventListener('click', function (){
+    fetchImages(pageToFetch++);
+});
+
+async function fetchImages(pageNum) {
+    try {
+        const response = await fetch('https://picsum.photos/v2/list?page=' + pageNum + '&limit=12');
+        if (!response.ok) {
+            throw new Error('네트워크 응답에 문제가 있습니다.');
+        }
+
+        const datas = await response.json();
+        makeImageList(datas);
+
+    } catch (error) {
+        console.error('데이터를 가져오는데 문제가 발생했습니다 :', error);
+    }
+}
+
+function makeImageList(datas) {
+    for(let i=0; i<datas.length; i+=3) {
+        imgDiv.innerHTML +=
+            "<div class='content-img-div-row'>" +
+            "<img src=" + datas[i].download_url + " alt=''>" +
+            "<img src=" + datas[i+1].download_url + " alt=''>" +
+            "<img src=" + datas[i+2].download_url + " alt=''>" +
+            "</div>"
+    }
+}
+
+window.addEventListener('scroll', () => {
+
+    console.log(window.innerHeight+"/"+document.documentElement.scrollTop+"/"+document.documentElement.offsetHeight);
+
+    if (window.innerHeight + document.documentElement.scrollTop + 750 >= document.documentElement.offsetHeight) {
+        if (!timer) {
+            timer = setTimeout(() => {
+                timer = null;
+                fetchImages(pageToFetch++);
+            }, 1000);
+        }
+    }
+});
