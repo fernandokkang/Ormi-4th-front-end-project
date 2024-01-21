30b53f30123e3046a7c5c4dc7c2956b901e21f9d
fernandokkang <spineattack@gmail.com>
2024-01-21 18:34:27 +0900

    refactor: delete not use const
    delete hover.js > const hoverBtnImg

--- js/hover.js
+++ js/hover.js
@@ -1,5 +1,4 @@
 const hoverBtn = document.querySelector('.hoverBtn');
-const hoverBtnImg = document.querySelector('.hoverBtnImg');
 
 hoverBtn.addEventListener('click', function (){
     window.scrollTo({top: 0, behavior: 'smooth'});
